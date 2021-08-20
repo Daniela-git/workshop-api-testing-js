0cb0a9aca8d9b70dcaa54f9d98480139ce0380e3
Daniela <63519335+Daniela-git@users.noreply.github.com>
2021-08-20 16:54:27 -0500

    get methods (#2)
    * post and get methods
    
    * patch and delete request
    
    * travis setup
    
    * mochawesome
    
    * mochawesome setup
    
    * config github auth
    
    * using get method
    
    * created GithubRequest.js
    
    with this all the github request are easier to control and don't have
    to repeat all the parameters
    
    * move the data to a file
    
    * finish the get tests
    
    * add mocha option in package.json
    
    * Download error (#1)
    
    * usign buffer
    
    * usign buffer
    
    * using svn url
    
    * without expect
    
    * ignore download repo
    
    * remove the exeptions from the package.json and add key in travis
    
    * change in download repo test
    
    * little fixes

---
+++ src/data/GetMethod.data.js
@@ -0,0 +1,21 @@
+getData = {
+  userInfo: {
+    name: 'Alejandro Perdomo',
+    company: 'Perficient Latam',
+    location: 'Colombia',
+  },
+  repoInfo: {
+    full_name: 'aperdomob/jasmine-awesome-report',
+    private: false,
+    description: 'An awesome html report for Jasmine',
+  },
+  objInfo: {
+    name: 'README.md',
+    path: 'README.md',
+    sha: '1eb7c4c6f8746fcb3d8767eca780d4f6c393c484',
+  },
+  md5Value: 'c95c49b42787e38e0d02793d605395f1',
+  md5RawFile: '3449c9e5e332f1dbb81505cd739fbf3f',
+};
+
+module.exports = getData;
---
+++ test/GithubApi.Respositories.test.js
@@ -0,0 +1,75 @@
+const md5 = require('md5');
+const agent = require('superagent');
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiSubset = require('chai-subset');
+const githubReq = require('../src/GithubRequest.js');
+const data = require('../src/data/GetMethod.data');
+chai.use(chaiSubset);
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'aperdomob';
+const repository = 'jasmine-awesome-report';
+
+describe('Trying Github Api GET methods', () => {
+  let user;
+  beforeEach(async () => {
+    const response = await githubReq.authGet(
+      `${urlBase}/users/${githubUserName}`
+    );
+    user = response.body;
+  });
+  it('get user name, company and location', () => {
+    expect(user).containSubset(data.userInfo);
+  });
+
+  describe('getting all repositories', () => {
+    let theRepo;
+    beforeEach(async () => {
+      const response = await githubReq.authGet(`${user.repos_url}`);
+      const repos = response.body;
+      theRepo = repos.find((repo) => repo.name === repository);
+    });
+    it('repository information', () => {
+      expect(theRepo).containSubset(data.repoInfo);
+    });
+
+    describe('Download a repository', () => {
+      let downloadRepo;
+      beforeEach(async () => {
+        const response = await githubReq.authGet(
+          `${theRepo.html_url}/archive/refs/heads/${theRepo.default_branch}.zip`
+        );
+        downloadRepo = response.body;
+      });
+      it('the repository should be downloaded', () => {
+        expect(md5(downloadRepo)).to.equal(data.md5Value);
+      });
+    });
+
+    describe('getting all the repo files', () => {
+      let theFile;
+      beforeEach(async () => {
+        const response = await githubReq.authGet(
+          `${theRepo.contents_url.replace('{' + '+path}', '')}`
+        );
+        const files = response.body;
+        theFile = files.find((file) => file.name === 'README.md');
+      });
+      it('should get the file info', () => {
+        expect(theFile).containSubset(data.objInfo);
+      });
+
+      describe('download the file from the repo', () => {
+        let rawFile;
+        beforeEach(async () => {
+          const response = await githubReq.authGet(theFile.download_url);
+          rawFile = response.body;
+        });
+        it('the file should be downloaded', () => {
+          expect(md5(rawFile)).to.eq(data.md5RawFile);
+        });
+      });
+    });
+  });
+});
