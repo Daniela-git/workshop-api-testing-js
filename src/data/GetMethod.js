7baea28da1feb410a096dd3f436ba56535c35f60
Daniela <dhiguitaa17@gmail.com>
2021-08-20 08:46:13 -0500

    move the data to a file

---
+++ src/data/GetMethod.js
@@ -0,0 +1,20 @@
+getData = {
+  userInfo: {
+    name: 'Alejandro Perdomo',
+    company: 'Perficient Latam',
+    location: 'Colombia',
+  },
+  repoInfo: {
+    full_name: 'aperdomob/jasmine-awesome-report',
+    private: false,
+    description: 'An awesome html report for Jasmine',
+  },
+  objInfo: {
+    name: 'README.md',
+    path: 'README.md',
+    sha: '1eb7c4c6f8746fcb3d8767eca780d4f6c393c484',
+  },
+  md5Value: 'c95c49b42787e38e0d02793d605395f1',
+};
+
+module.exports = getData;
--- test/GithubApi.Respositories.test.js
+++ test/GithubApi.Respositories.test.js
@@ -5,19 +5,15 @@ const chai = require('chai');
 const expect = require('chai').expect;
 const chaiSubset = require('chai-subset');
 const githubReq = require('../src/GithubRequest.js');
+const data = require('../src/data/GetMethod');
 chai.use(chaiSubset);
 
 const urlBase = 'https://api.github.com';
 const githubUserName = 'aperdomob';
 const repository = 'jasmine-awesome-report';
 
-describe.only("GithubApi's GET method tests", () => {
+describe("GithubApi's GET method tests", () => {
   let user;
-  let userInfo = {
-    name: 'Alejandro Perdomo',
-    company: 'Perficient Latam',
-    location: 'Colombia',
-  };
   beforeEach(async () => {
     const response = await githubReq.authGet(
       `${urlBase}/users/${githubUserName}`
@@ -25,28 +21,21 @@ describe.only("GithubApi's GET method tests", () => {
     user = response.body;
   });
   it('get user name, company and location', async () => {
-    expect(user).containSubset(userInfo);
+    expect(user).containSubset(data.userInfo);
   });
 
   describe('getting repositories', () => {
     let theRepo;
-    let repoInfo = {
-      full_name: 'aperdomob/jasmine-awesome-report',
-      private: false,
-      description: 'An awesome html report for Jasmine',
-    };
     beforeEach(async () => {
       const response = await githubReq.authGet(`${user.repos_url}`);
       const repos = response.body;
       theRepo = repos.find((repo) => repo.name === repository);
     });
     it('repository information', async () => {
-      expect(theRepo).containSubset(repoInfo);
+      expect(theRepo).containSubset(data.repoInfo);
     });
     describe('Download a repository', () => {
-      let md5Value = 'c95c49b42787e38e0d02793d605395f1';
       let downloedRepo;
-
       beforeEach(async () => {
         const response = await githubReq.authGet(
           `${theRepo.html_url}/archive/refs/heads/${theRepo.default_branch}.zip`
@@ -54,17 +43,12 @@ describe.only("GithubApi's GET method tests", () => {
         downloedRepo = response.body;
       });
       it('the repository should be downloaded', async () => {
-        expect(md5(downloedRepo)).to.equal(md5Value);
+        expect(md5(downloedRepo)).to.equal(data.md5Value);
       });
     });
 
     describe("getting all the repo's files", () => {
       let theFile;
-      const objInfo = {
-        name: 'README.md',
-        path: 'README.md',
-        sha: '1eb7c4c6f8746fcb3d8767eca780d4f6c393c484',
-      };
       beforeEach(async () => {
         const response = await githubReq.authGet(
           `${theRepo.contents_url.replace('{' + '+path}', '')}`
@@ -73,7 +57,7 @@ describe.only("GithubApi's GET method tests", () => {
         theFile = files.find((file) => file.name === 'README.md');
       });
       it('should get the file info', () => {
-        expect(theFile).containSubset(objInfo);
+        expect(theFile).containSubset(data.objInfo);
       });
     });
   });
